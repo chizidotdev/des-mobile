5c4b830ff5b06f98f9040daec47dae9d6a86f72e
chiziivictor <chizivwokoma@gmail.com>
2022-07-30 19:54:29 +0100

    edit app.json target files

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import AuthForm from "../components/AuthForm";
 import LinkUI from "../components/UI/Link";
